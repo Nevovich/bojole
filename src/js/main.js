18d2680715033187b7a9f62a57ff03390b453a3c
Nevovich <nevovich@outlook.com>
2022-07-06 21:16:59 +0500

    4. Визуал готов, кроме одного блока, адаптив тоже

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+if ($(window).width() > 991) {
+    $( ".form-block-img img" ).attr("src","img/form/girl-clear.png");
+}
+// if ($(window).width() < 768) {
+//     $('.slider-block').slick({
+//         slidesToShow: 2
+//       });
+// }
+// $('.slider-item').slick();
