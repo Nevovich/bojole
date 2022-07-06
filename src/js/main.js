e815677a0ff9b4ec71affde7eafb8c43340eab05
Nevovich <nevovich@outlook.com>
2022-07-06 22:31:08 +0500

    5. Доделан весь визуал, включена метрика, плавно

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,15 @@
 if ($(window).width() > 991) {
     $( ".form-block-img img" ).attr("src","img/form/girl-clear.png");
-}
+};
 // if ($(window).width() < 768) {
 //     $('.slider-block').slick({
 //         slidesToShow: 2
 //       });
 // }
 // $('.slider-item').slick();
+$('#headerNavbar a[href^="#"]').click(function (){
+    let elementClick = $(this).attr("href");
+    let destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
+    return false;
+});
